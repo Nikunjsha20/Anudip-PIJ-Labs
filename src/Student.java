e72cab5e2476ee9e5681e06b13f5843766742324
Nikunjsha20 <nikunjs456@gmail.com>
2025-05-02 13:34:35 +0530

    Uploaded first Assignment of Session2 lab

---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    private String studentName, collegeName;
+    private int studentID;
+    public static void main(String [] args){
+        System.out.println("Successful");
+    }
+}
