4c183ff91bfbe757f25b9289280474df1167eaee
Nikunjsha20 <nikunjs456@gmail.com>
2025-05-02 13:52:42 +0530

    Uploaded Second Assignment

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,12 @@
+class Employee{
+    protected int id, age;
+    protected String name;
+    protected boolean isPermanent;
+    //age = 35.5; //java: <identifier> expected
+    protected float agE = (float) age;
+    public static void main(String[] args){
+        System.out.println("Successfully Started");
+    }
 
+
+}
