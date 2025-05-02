cec092a4795aecc2bd177f7c37abe0bbd2f23ab5
Nikunjsha20 <nikunjs456@gmail.com>
2025-05-02 14:18:33 +0530

    Person.java Third Assignment of Session2 lab Uploaded.

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+
+public class Person {
+    String name = "Nikunj";
+    int age = 26, salary= 10000;
+    public static void main(String[] args){
+        System.out.println("Test Successful");
+    }
+
+
+
+}
