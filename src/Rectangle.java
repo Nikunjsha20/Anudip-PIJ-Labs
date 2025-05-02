e4b18cefebfa8b562d5d593b24544b81b112b53d
Nikunjsha20 <nikunjs456@gmail.com>
2025-05-02 15:05:39 +0530

    Rectangle fourth Assignment of Session 2 lab

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    double height, width;
+
+    enum Color{
+        RED, GREEN, BLUE
+    }
+
+    Color boxcolor;
+    public static void main(String [] args){
+        System.out.println(Color.BLUE);
+    }
+
+}
